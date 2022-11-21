778cd0dc44602d6d8c800aa916618a4066c0e744
Ivan Alba <ivan.alba@mac.com>
2022-11-21 01:16:10 -0400

    fix dashboard and bottom bar

--- src/app/pages/feed/feed-routing.module.ts
+++ src/app/pages/feed/feed-routing.module.ts
@@ -1,4 +1,4 @@
-import { importType } from '@angular/compiler/src/output/output_ast';
+//import { importType } from '@angular/compiler/src/output/output_ast';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
